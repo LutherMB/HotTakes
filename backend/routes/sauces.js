4a8d6810cf0ddb277bcdcb89d0e87954725fc8fe
Luther M'basse <39866739+LutherMB@users.noreply.github.com>
2022-08-24 17:03:34 +0200

    projet final

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-const stuffCtrl = require('../controllers/stuff');
+const stuffCtrl = require('../controllers/stuff'); // Je récupère mon controller "stuff"
 
-router.post('/', auth, multer, stuffCtrl.createThing);
+router.post('/', auth, multer, stuffCtrl.createThing); // en cas de POST sur l'url ./, je lance la fonction "createThing" de mon controller
 router.post('/:id/like', stuffCtrl.likeSauce);
 router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
